425f270ce2d7f0435c1c87d3808699862975eef8
Steeve R <st2diio@protonmail.ch>
2020-03-10 09:22:59 +0100

    Upload basics

---
+++ pwa-server/src/server.ts
@@ -0,0 +1,41 @@
+  
+import express from "express";
+import socketIO from "socket.io";
+import { createServer } from "http";
+import { config } from "dotenv";
+
+config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+const server = createServer(app);
+const io = socketIO(server);
+let players = [];
+
+app.get("/", (_, res) => {
+	res.send("hello fellows");
+});
+
+io.on("connection", socket => {
+	console.log("new connection");
+	socket.emit("event::hello");
+
+	socket.on("event::initialize", payload => {
+		if (players.length >= 2) {
+			socket.emit("event::gameFull");
+			return;
+		}
+
+		players.push(payload);
+		console.log("new name received: ", payload.nickname);
+
+		if (players.length === 2) {
+			io.emit("event::gameStart");
+		}
+	});
+});
+
+server.listen(PORT, () => {
+	console.log(`Server ready at ... ${PORT}`);
+});
